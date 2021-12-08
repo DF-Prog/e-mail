4e011c7c1c060b0bc89bcc8e4637bddbd676395a
DF-Prog <95499300+DF-Prog@users.noreply.github.com>
2021-12-08 09:05:10 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Validação de E-mail</title>
+  <script src="email.js" type="text/javascript"></script>
+</head>
+<body>
+  <input type="text" value="contato@wsa.net.br" autofocus
+  onblur="valEmail(this)" onkeyup="corEmail(this);"/>
+</body>
+</html>
